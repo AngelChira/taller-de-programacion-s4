92d24be00b34a22a9ca2dddacdff96d2f7cdf227
Angel Chira <125518726+AngelChira@users.noreply.github.com>
2023-09-09 15:51:14 -0500

    Actividad de la Semana 04

---
+++ src/semana04/Semana04.java
@@ -0,0 +1,27 @@
+package semana04;
+
+import java.util.Scanner;
+
+public class Semana04 {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Ingrese el numero de horas: ");
+        byte numeroHoras = teclado.nextByte();
+        float montoPagar =0;
+        switch (numeroHoras) {
+            case 1, 2:
+                montoPagar = (float) (numeroHoras * 5);
+                break;
+            case 3, 4, 5:
+                montoPagar = (float) (5*2 + 4*(numeroHoras - 2));
+                break;
+            case 6, 7, 8, 9, 10:
+                montoPagar = (float) (5*2 + 4*3 + 3*(numeroHoras - 5));
+            default:
+                montoPagar = (float) (5*2 + 4*3 + 3*5 + (numeroHoras - 10));
+        }
+        System.out.printf("El monto total a pagar es S/%.2f soles por %d horas. %n",montoPagar,numeroHoras);
+    }
+    
+}
